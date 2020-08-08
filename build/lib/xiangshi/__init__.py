00b8b1384cf38627b85332035933fdb7e3685e53
kiwirafe <kiwirafe@gmail.com>
2020-08-08 13:49:07 +1200

    v1.2.0

--- build/lib/xiangshi/__init__.py
+++
@@ -1,8 +0,0 @@
-from xiangshi.main import calculator
-
-calculator = calculator()
-cal = calculator.cal
-input2list = calculator.input2list
-dict2file = calculator.dict2file
-GetTF = calculator.GetTF
-GetIDF = calculator.GetIDF

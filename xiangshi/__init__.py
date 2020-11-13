bdb44d18950ebb3ef1d5f8c1f236d91f8bae5fe3
kiwirafe <kiwirafe@gmail.com>
2020-11-14 12:15:47 +1300

    v3.0.0b2

--- xiangshi/__init__.py
+++ xiangshi/__init__.py
@@ -1,12 +1,7 @@
 from .main import calculator
-<<<<<<< HEAD
 from .formats import FormatFuncs as fmt
 from development import calculator as dev
 from .weight import calculator as ext
-=======
-from .formats import formats
-from .weight import calculator as extra
->>>>>>> e97ffbae5c3dbae9a07393eba0bfd095d292b62a
 
 calculator = calculator()
 
@@ -14,30 +9,16 @@ cossim = calculator.cossim
 minhash = calculator.minhash
 simhash = calculator.simhash
 
-<<<<<<< HEAD
 noweight = ext()
 noweight.weight = None
 
 tfweight = ext()
-=======
-noweight = extra()
-noweight.weight = None
-
-tfweight = extra()
->>>>>>> e97ffbae5c3dbae9a07393eba0bfd095d292b62a
 tfweight.weight = "TF"
 
 __all__ = [
     'calculator',
-<<<<<<< HEAD
     'fmt',
     'dev',
     'noweight',
     'tfweight'
 ]
-=======
-    'formats',
-    'noweight',
-    'tfweight'
-]
->>>>>>> e97ffbae5c3dbae9a07393eba0bfd095d292b62a
